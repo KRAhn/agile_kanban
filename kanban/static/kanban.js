c5ee9c12a6a7fe21f16eeee5a89512ce3d27a9d0
acuros <acuros@kaist.ac.kr>
2013-11-25 12:59:36 +0900

    template architecture init

---
+++ kanban/static/kanban.js
@@ -0,0 +1,3 @@
+/**
+ * Created by acuros_ on 13. 11. 25..
+ */
--- kanban/views.py
+++ kanban/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
